77476729251ad54bdc52183ca504f7837a70bf95
rachid <rachid98baghrosse@gmail.com>
2021-07-07 13:06:55 +0200

    express server and socket io set up

---
+++ video-talker-server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const socket =require('socket.io')
+
+const PORT = 5000;
+const app = express()
+
+const server = app.listen(PORT,()=>{
+    console.log(`server is listening on port ${PORT}`)
+    console.log(`http://localhost:${PORT}`)
+})
+
+//define our sockets io
+const io = socket(server,{
+    cors:{
+        origin:'*',
+        methods:['GET','POST']
+    }
+})
+
+//white our call back if event is called (connection event for example)
+io.on('connection',(socket)=>{
+    socket.emit('connection',null)
+    console.log('new users connected')
+    console.log(socket.io)
+})
